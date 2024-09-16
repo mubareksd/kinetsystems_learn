449e0adaa1d15de1b82b6ad2a42ab232402768ab
Mubarek Seid Juhar <mubareksd@gmail.com>
2024-09-16 20:57:22 +0300

    feat: localization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["en", "am", "ar", "ti"],
+    defaultLocale: "en",
+  },
+};
 
 export default nextConfig;
